187af1461a535dd7dd3ad9ce39ac819844ead6cf
SultanMashukov1 <mashukov.s@leama.agency>
2018-03-29 10:56:04 +0300

    page "about" was adapted

--- about/index.php
+++ about/index.php
@@ -19,32 +19,71 @@ $APPLICATION->SetTitle("Для Вас");
                         <div class="heading">
                             <h2 class="heading__title"><? $APPLICATION->IncludeFile(SITE_DIR . 'include/about/our_value/title.php'); ?></h2>
                         </div>
-                        <div class="about-content__wrap">
-                            <ul class="about-list">
-                                <li class="about-list__item">
-                                    <p>Фабрика разрабатывает и изготавливает авторскую, эксклюзивную одежду из натурального меха овчины (мутон) премиум класса, астрагана, каракуля, норки, бобра, соболя, кожаных и замшевых изделий, дубленок высочайшего
-                                        качества</p>
-                                </li>
-                                <li class="about-list__item">
-                                    <p>Наш опыт и мастерство, внимание к каждой детали, использование только лучших мехов и кожи, являются гарантом качества наших изделий. Меховые изделия бренда «MELICH» - это синоним качества и правильной цены.</p>
-                                </li>
-                                <li class="about-list__item">
-                                    <p>Стараемся развиваться не только сохраняя свое высокое качество, но и улучшая его.</p>
-                                </li>
-                            </ul>
-                            <ul class="about-list">
-                                <li class="about-list__item">
-                                    <p>Шубы Компании «MELICH» – это женственный, элегантный силуэт, стильные шубы на каждый день, эксклюзивные меховые изделия для особого выхода, разнообразная палитра меха, а также цветовая гамма, которая не оставит
-                                        равнодушных самых избалованных меховых гурманов.</p>
-                                </li>
-                                <li class="about-list__item">
-                                    <p>Мы уделяем особое внимание соблюдению международных стандартов по защите животных, покупая только качественное сырье у сертифицированных поставщиков на мировых аукционах.</p>
-                                </li>
-                                <li class="about-list__item">
-                                    <p>При создании нашей очередной коллекции, мы учитываем все тенденции моды, которые идеально сочетаются с инновациями и современным стилем</p>
-                                </li>
-                            </ul>
-                        </div>
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.list",
+                            "blocks_our_value",
+                            array(
+                                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "CACHE_FILTER" => "N",
+                                "CACHE_GROUPS" => "Y",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_TYPE" => "A",
+                                "CHECK_DATES" => "Y",
+                                "DETAIL_URL" => "",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "FILTER_NAME" => "",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                                "IBLOCK_ID" => "8",
+                                "IBLOCK_TYPE" => "content",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "INCLUDE_SUBSECTIONS" => "Y",
+                                "MESSAGE_404" => "",
+                                "NEWS_COUNT" => "20",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "PAGER_TITLE" => "Новости",
+                                "PARENT_SECTION" => "",
+                                "PARENT_SECTION_CODE" => "",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "SET_BROWSER_TITLE" => "N",
+                                "SET_LAST_MODIFIED" => "N",
+                                "SET_META_DESCRIPTION" => "N",
+                                "SET_META_KEYWORDS" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SET_TITLE" => "N",
+                                "SHOW_404" => "N",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_BY2" => "SORT",
+                                "SORT_ORDER1" => "DESC",
+                                "SORT_ORDER2" => "ASC",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "COMPONENT_TEMPLATE" => "blocks_our_value"
+                            ),
+                            false
+                        );?>
                     </div>
                 </div>
             </div>
@@ -63,70 +102,70 @@ $APPLICATION->SetTitle("Для Вас");
                 <h2 class="heading__title"><? $APPLICATION->IncludeFile(SITE_DIR . 'include/about/guarantees/title.php'); ?></h2>
             </div>
             <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"our_awards", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "1",
-		"IBLOCK_TYPE" => "content",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "20",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "our_awards"
-	),
-	false
-);?>
+                "bitrix:news.list",
+                "our_awards",
+                array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "ADD_SECTIONS_CHAIN" => "N",
+                    "AJAX_MODE" => "N",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "CACHE_FILTER" => "N",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_TYPE" => "A",
+                    "CHECK_DATES" => "Y",
+                    "DETAIL_URL" => "",
+                    "DISPLAY_BOTTOM_PAGER" => "Y",
+                    "DISPLAY_DATE" => "Y",
+                    "DISPLAY_NAME" => "Y",
+                    "DISPLAY_PICTURE" => "Y",
+                    "DISPLAY_PREVIEW_TEXT" => "Y",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "FIELD_CODE" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "FILTER_NAME" => "",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "IBLOCK_ID" => "1",
+                    "IBLOCK_TYPE" => "content",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                    "INCLUDE_SUBSECTIONS" => "Y",
+                    "MESSAGE_404" => "",
+                    "NEWS_COUNT" => "20",
+                    "PAGER_BASE_LINK_ENABLE" => "N",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "N",
+                    "PAGER_SHOW_ALWAYS" => "N",
+                    "PAGER_TEMPLATE" => ".default",
+                    "PAGER_TITLE" => "Новости",
+                    "PARENT_SECTION" => "",
+                    "PARENT_SECTION_CODE" => "",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "PROPERTY_CODE" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "SET_BROWSER_TITLE" => "N",
+                    "SET_LAST_MODIFIED" => "N",
+                    "SET_META_DESCRIPTION" => "N",
+                    "SET_META_KEYWORDS" => "N",
+                    "SET_STATUS_404" => "N",
+                    "SET_TITLE" => "N",
+                    "SHOW_404" => "N",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_ORDER2" => "ASC",
+                    "STRICT_SECTION_CHECK" => "N",
+                    "COMPONENT_TEMPLATE" => "our_awards"
+                ),
+                false
+            );?>
         </div>
     </section>
     <!--End awards-->
---
+++ local/templates/.default/components/bitrix/news.list/blocks_our_value/template.php
@@ -0,0 +1,52 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+use Lema\Common\Helper as H,
+    Lema\Template\TemplateHelper as TH,
+    Bitrix\Main\Localization\Loc;
+if(empty($arResult['ITEMS']))
+    return ;
+Loc::loadMessages(__FILE__);
+$this->setFrameMode(true);
+$data = new TH($this);
+
+$list1 = array();
+$list2 = array();
+
+foreach ($data->items() as $key=>$item)
+{
+    if ($key % 2 != 0)
+    {
+        $list1[] = $item;
+    }
+    else
+    {
+        $list2[] = $item;
+    }
+}
+?>
+<div class="about-content__wrap">
+    <ul class="about-list">
+        <?foreach ($list2 as $listItem):?>
+            <li class="about-list__item">
+                <p><?=$listItem->previewText()?></p>
+            </li>
+        <?endforeach;?>
+    </ul>
+    <ul class="about-list">
+        <?foreach ($list1 as $listItem):?>
+            <li class="about-list__item">
+                <p><?=$listItem->previewText()?></p>
+            </li>
+        <?endforeach;?>
+    </ul>
+</div>
