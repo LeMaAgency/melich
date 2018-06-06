eb780d8de4f27cb1b5d7223d1a054012dbf81690
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-06-06 16:08:02 +0300

    db and submenu

--- local/templates/.default/components/bitrix/menu/top_menu_inner/template.php
+++ local/templates/.default/components/bitrix/menu/top_menu_inner/template.php
@@ -13,30 +13,50 @@ Loc::loadMessages(__FILE__);
         <?= Loc::getMessage('LEMA_MENU_INNER_TITLE'); ?>
     </div>
     <ul class="main-nav__list">
-        <?
+        <?$checkTwoLevel = false;
         foreach ($arResult as $arItem):
             if ($arParams['MAX_LEVEL'] == 1 && $arItem['DEPTH_LEVEL'] > 1) {
                 continue;
             }
-            ?>
-            <? if ($arItem['SELECTED']): ?>
-                <li class="main-nav__item  is-active">
-                    <a href="<?= $arItem['LINK']; ?>"
-                       title="<?= $arItem['TEXT']; ?>"
-                       class="main-nav__link">
-                        <?= $arItem['TEXT']; ?>
-                    </a>
-                </li>
-            <? else: ?>
-                <li class="main-nav__item">
-                    <a href="<?= $arItem['LINK']; ?>"
-                       title="<?= $arItem['TEXT']; ?>"
-                       class="main-nav__link">
-                        <?= $arItem['TEXT']; ?>
-                    </a>
-                </li>
-            <? endif ?>
 
+            ?>
+             <?if ($arItem['DEPTH_LEVEL'] == 1):?>
+                 <?if($checkTwoLevel){?>
+                    </ul>
+                 <?$checkTwoLevel=false;}?>
+                    <? if ($arItem['SELECTED']):
+                        $selectedCheck = true;
+                    ?>
+                        <li class="main-nav__item  is-active">
+                            <a href="<?= $arItem['LINK']; ?>"
+                               title="<?= $arItem['TEXT']; ?>"
+                               class="main-nav__link">
+                                <?= $arItem['TEXT']; ?>
+                            </a>
+                        </li>
+                    <? else:
+                        $selectedCheck = false;
+                    ?>
+                        <li class="main-nav__item">
+                            <a href="<?= $arItem['LINK']; ?>"
+                               title="<?= $arItem['TEXT']; ?>"
+                               class="main-nav__link">
+                                <?= $arItem['TEXT']; ?>
+                            </a>
+                        </li>
+                    <? endif; ?>
+                <? elseif($selectedCheck): ?>
+                <?if(!$checkTwoLevel){?>
+                    <ul>
+                <?$checkTwoLevel = true;}?>
+                        <li <? if ($arItem['SELECTED']){?>class="is-active"<?}?>>
+                            <a href="<?= $arItem['LINK']; ?>"
+                               title="<?= $arItem['TEXT']; ?>"
+                               class="main-nav__link_sub">
+                                <?= $arItem['TEXT']; ?>
+                            </a>
+                        </li>
+                <? endif ?>
         <? endforeach ?>
 
     </ul>
