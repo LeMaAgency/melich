6f78618e285f8341a0d6235e80728c56d2034df4
radiofunt <radiofunt@gmail.com>
2018-06-19 20:58:10 +0300

    Телефон

--- include/header/phone_link.php
+++ include/header/phone_link.php
@@ -1,1 +1,1 @@
-<a href="tel:81234567890" title="Позвонить" class="header__main__phone__number__control">8 (123) 456-78-90</a>
+<a href="tel:89282656333" title="Позвонить" class="header__main__phone__number__control">8 (928) 265-63-33</a>
