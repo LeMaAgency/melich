57c1ea296f3abfd98be40c7d887762cc859a8e08
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-06-21 10:11:37 +0300

    reviews for main page

--- local/templates/.default/components/bitrix/news.list/reviews_list_main/lang/ru/template.php
+++ local/templates/.default/components/bitrix/news.list/reviews_list_main/lang/ru/template.php
@@ -1,3 +1,4 @@
 <?
 $MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
-?>
+$MESS["LEMA_REVIEWS_LIST_MAIN_MORE_INFO"] = "Читать далее";
+$MESS["LEMA_REVIEWS_LIST_MAIN_ALL"] = "Читать все отзывы";
--- local/templates/.default/components/bitrix/news.list/reviews_list_main/template.php
+++ local/templates/.default/components/bitrix/news.list/reviews_list_main/template.php
@@ -1,5 +1,4 @@
-<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
-{
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 /** @var array $arParams */
@@ -15,35 +14,83 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
 $data = new \Lema\Template\TemplateHelper($this);
 
 ?>
-<? if($data->itemCount()): ?>
+<? /* if($data->itemCount()): */ ?><!--
     <div class="page__index__inf__tabs__block__comment__list">
-        <? foreach($data->items() as $item): ?>
+        <? /* foreach($data->items() as $item): */ ?>
 
-            <div class="page__index__inf__tabs__block__advantages__list__item" <?=$item->editId();?>>
+            <div class="page__index__inf__tabs__block__advantages__list__item" <? /*=$item->editId();*/ ?>>
                 <div class="page__index__inf__tabs__block__advantages__list__item__img">
-                    <img src="<?=$item->previewPicture();?>" alt="<?=$item->getName();?>">
+                    <img src="<? /*=$item->previewPicture();*/ ?>" alt="<? /*=$item->getName();*/ ?>">
                 </div>
                 <div class="page__index__inf__tabs__block__advantages__list__item__text">
                         <span class="page__index__inf__tabs__block__advantages__list__item__text__control">
-                            <?=$item->getName();?>
+                            <? /*=$item->getName();*/ ?>
                         </span>
                 </div>
             </div>
 
-        <? endforeach; ?>
+        <? /* endforeach; */ ?>
     </div>
 
     <div class="page__index__inf__tabs__block__comment__btn">
         <a href="/" title="Читать все отзывы"
            class="page__index__inf__tabs__block__comment__btn__control">Читать все отзывы</a>
     </div>
+--><? /* endif; */ ?>
+<? if ($data->itemCount()): ?>
+    <div class="page__index__inf__tabs__block__comment__list">
+        <? foreach ($data->items() as $key => $item): ?>
+            <? if ($key % 2 == 0) { ?>
+                <div class="page__index__inf__tabs__block__comment__list__group">
+            <? } ?>
+            <div class="page__index__inf__tabs__block__comment__list__item" <?= $item->editId(); ?>>
+                <div class="page__index__inf__tabs__block__comment__list__item__name">
+                        <span>
+                            <?= $item->getName(); ?>
+                        </span>
+                </div>
+                <div class="page__index__inf__tabs__block__comment__list__item__city">
+                        <span>
+                            <?= $item->propVal("CITY"); ?>
+                        </span>
+                </div>
+                <div class="page__index__inf__tabs__block__comment__list__item__text">
+                        <span>
+                            <?= $item->previewText(); ?>
+                        </span>
+                </div>
+                <div class="page__index__inf__tabs__block__comment__list__item__btn">
+                    <a href=""
+                       title="<?= Loc::getMessage("LEMA_REVIEWS_LIST_MAIN_MORE_INFO"); ?>"
+                       class="page__index__inf__tabs__block__comment__list__item__btn__control">
+                        <?= Loc::getMessage("LEMA_REVIEWS_LIST_MAIN_MORE_INFO"); ?>
+                    </a>
+                </div>
+            </div>
+            <? if (($key + 1) % 2 == 0 || ($key+1) == count($data->items())) { ?>
+                </div>
+            <? } ?>
+        <? endforeach; ?>
+    </div>
+
+    <div class="page__index__inf__tabs__block__comment__btn">
+        <a href="/"
+           title="<?= Loc::getMessage("LEMA_REVIEWS_LIST_MAIN_ALL"); ?>"
+           class="page__index__inf__tabs__block__comment__btn__control">
+            <?= Loc::getMessage("LEMA_REVIEWS_LIST_MAIN_ALL"); ?>
+        </a>
+    </div>
 <? endif; ?>
 
 <!-- list -->
-<div class="page__index__inf__tabs__block__comment__list">
+<!--<div class="page__index__inf__tabs__block__comment__list">
     <div class="page__index__inf__tabs__block__comment__list__group">
         <div class="page__index__inf__tabs__block__comment__list__item">
             <div class="page__index__inf__tabs__block__comment__list__item__name">
@@ -112,6 +159,6 @@ $data = new \Lema\Template\TemplateHelper($this);
             </div>
         </div>
     </div>
-</div>
+</div>-->
 <!-- list end -->
 
